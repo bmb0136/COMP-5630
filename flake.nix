a542ae205e81552f4a38a56f5780542573152922
Brandon Buckley <brandonmb0136@gmail.com>
2026-01-28 11:27:11 -0600

    Add A1 files

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              (pkgs.python3.withPackages (
+                pp: with pp; [
+                  numpy
+                  matplotlib
+                  jupyter
+                  ipympl
+                ]
+              ))
+            ];
+          };
+        };
+    };
+}
