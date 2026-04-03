ebec73dc7cde723c1d32016a49ce1bb90e109fa9
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-03 11:27:27 -0500

    Do a3 part 1

--- flake.nix
+++ flake.nix
@@ -28,6 +28,8 @@
                   jupyter
                   ipympl
                   torch
+                  datasets
+                  gensim
                 ]
               ))
             ];
