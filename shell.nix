963ba456d7eb16227d60eca58dffac9fde173117
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-04 00:17:45 -0500

    done

--- shell.nix
+++ shell.nix
@@ -5,20 +5,27 @@
   ...
 }:
 pkgs.mkShell {
-  packages = [
-    (python3.withPackages (
-      pp: with pp; [
-        numpy
-        matplotlib
-        pandas
-        scikit-learn
-        openpyxl
-        jupyter
-        ipympl
-        datasets
-        gensim
-        (tensorflow.override { inherit cudaSupport; })
-      ]
-    ))
-  ];
+  packages =
+    let
+      tensorflow = python3.pkgs.tensorflow.override { inherit cudaSupport; };
+    in
+    [
+      (python3.withPackages (pp: [
+        pp.numpy
+        pp.matplotlib
+        pp.pandas
+        pp.scikit-learn
+        pp.openpyxl
+        pp.jupyter
+        pp.ipympl
+        pp.datasets
+        pp.gensim
+        tensorflow
+        (pp.keras.override (prev: {
+          inherit tensorflow;
+          tf2onnx = prev.tf2onnx.override { inherit tensorflow; };
+        }))
+        pp.fasttext
+      ]))
+    ];
 }
